a746d3fbfcb7c2f0a3ac047a42fdb9bf6b05ef44
Paul Zabelin <p.zabelin@gmail.com>
2016-02-19 22:53:43 -0800

    add vector projection spec

---
+++ Example/Tests/VectorProjectionSpec.swift
@@ -0,0 +1,17 @@
+import Quick
+import Nimble
+import simd
+
+class VectorProjectionSpec: QuickSpec {
+    override func spec() {
+        describe("vector projection") { () -> () in
+                it("should be 0 orthogonally", closure: { () -> () in
+                    let v12 = vector_float2(1,0)
+                    let v23 = vector_float2(0,1)
+                    let p = project(v12, v23)
+                    expect(p.x) == 0
+                    expect(p.y) == 0
+                })
+        }
+    }
+}
