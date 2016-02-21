0098fcab2cfce4751998cb510efe8e218fc02008
Paul Zabelin <pzabelin+git@gmail.com>
2016-02-20 23:18:49 -0800

    split spec to extract BasisVectorSpec.swift

---
+++ Example/Tests/BasisVectorSpec.swift
@@ -0,0 +1,41 @@
+import Quick
+import Nimble
+import simd
+@testable import PerspectiveTransform
+
+class BasisSpec: QuickSpec {
+    override func spec() {
+        let start = Quadrilateral(CGRect(origin: CGPointZero, size: CGSize(width: 152, height: 122)))
+        let destination = Quadrilateral(
+            CGRect(
+                origin: CGPoint(x: 100, y: 100),
+                size: CGSize(width: 200, height: 200)
+            )
+        )
+
+
+        context("multiply adj by vector") {
+            it("should match expected") {
+                let adjM = float3x3([-122, -152, 18544, 122, 0, 0, 0, 152, 0])
+                let vector = float3([152, 122, 1])
+                let result = adjM * vector
+                let expected = float3([-18544, 18544, 18544])
+                expect(result) == expected
+            }
+        }
+
+        context("basis") {
+            it("should match fiddle") {
+                let startBasis = normalize(float3x3([0, 2818688, 0, 0, 0, 2262368, -18544, 18544, 18544]))
+                let result = basis(start)
+                expect(result) ≈ startBasis ± 0.5
+            }
+
+            it("should work for destination") {
+                let destBasis = normalize(float3x3([-4000000, 12000000, 4000000, -4000000, 4000000, 12000000, -40000, 40000, 40000]))
+                let result = basis(destination)
+                expect(result) ≈ destBasis ± 0.5
+            }
+        }
+    }
+}
