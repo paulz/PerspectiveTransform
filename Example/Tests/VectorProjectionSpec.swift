ec9e3b3541bd9ea6e09f25c610ba8c949115d7ee
Paul Zabelin <pzabelin+git@gmail.com>
2016-02-20 22:11:45 -0800

    remove placeholder reformat code

--- Example/Tests/VectorProjectionSpec.swift
+++ Example/Tests/VectorProjectionSpec.swift
@@ -5,13 +5,13 @@ import simd
 class VectorProjectionSpec: QuickSpec {
     override func spec() {
         describe("vector projection") { () -> () in
-                it("should be 0 orthogonally", closure: { () -> () in
-                    let v12 = vector_float2(1,0)
-                    let v23 = vector_float2(0,1)
-                    let p = project(v12, v23)
-                    expect(p.x) == 0
-                    expect(p.y) == 0
-                })
+            it("should be 0 orthogonally", closure: { () -> () in
+                let v12 = vector_float2(1,0)
+                let v23 = vector_float2(0,1)
+                let p = project(v12, v23)
+                expect(p.x) == 0
+                expect(p.y) == 0
+            })
         }
     }
 }
