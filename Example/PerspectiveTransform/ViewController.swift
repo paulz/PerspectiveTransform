62975e013ff768e3e66ec9c0e74cc5848727ca10
Paul Zabelin <pzabelin+git@gmail.com>
2016-02-20 21:47:17 -0800

    reset anchor point to zero before any transformations

--- Example/PerspectiveTransform/ViewController.swift
+++ Example/PerspectiveTransform/ViewController.swift
@@ -13,20 +13,37 @@ import QuartzCore
 
 class ViewController: UIViewController {
 
+    // MARK: - Outlets
 
-  @IBOutlet var cornerViews: [UIView]!
+    @IBOutlet var cornerViews: [UIView]!
     @IBOutlet weak var transView: UIView!
     @IBOutlet weak var startView: UIView!
     @IBOutlet weak var destView: UIView!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-      self.transView.frame = self.destView.frame
-      self.transView.layer.anchorPoint = CGPointZero
-    }
+
+    // MARK: - UIViewController
 
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        anchorAtZeroPoint()
         updatePosition()
+    }
+
+    @IBAction func didPan(recognizer: UIPanGestureRecognizer) {
+        let controlPoint = recognizer.view!
+        let translation = recognizer.translationInView(controlPoint.superview)
+        let transform = CGAffineTransformMakeTranslation(translation.x, translation.y)
+        controlPoint.center = CGPointApplyAffineTransform(controlPoint.center, transform)
+        recognizer.setTranslation(CGPointZero, inView:controlPoint.superview)
+        updatePosition()
+    }
+
+
+    // MARK: - private
+
+    func anchorAtZeroPoint() {
+        let rect = transView.frame
+        transView.layer.anchorPoint = CGPointZero
+        transView.frame = rect
     }
 
     func updatePosition() {
@@ -36,19 +53,7 @@ class ViewController: UIViewController {
         let projection = general2DProjection(start, to: quad)
         let expanded = expandNoZ(expandNoZ(projection))
         let transform3D = transform(expanded)
-
-        self.transView.layer.transform = transform3D
+        transView.layer.transform = transform3D
     }
-
-  @IBAction func didPan(recognizer: UIPanGestureRecognizer) {
-    let controlPoint = recognizer.view!
-    let translation = recognizer.translationInView(view)
-    controlPoint.center = CGPoint(x: controlPoint.center.x + translation.x,
-      y: controlPoint.center.y + translation.y)
-    recognizer.setTranslation(CGPointZero, inView: view)
-    updatePosition()
-
-  }
-
 }
 
