26f7e0c0876cdf70cda1f2709f432b64e745bf9d
Paul Zabelin <pzabelin+git@gmail.com>
2018-01-04 07:12:45 -0800

    refactor beCloseTo

--- Example/Tests/SpecHelper.swift
+++ Example/Tests/SpecHelper.swift
@@ -10,35 +10,13 @@ import simd
 import Nimble
 @testable import PerspectiveTransform
 
-
-public func ==(lhs: float2x2, rhs: float2x2) -> Bool {
-    return matrix_equal(lhs, rhs)
-//    return matrix_almost_equal_elements(lhs.cmatrix, rhs.cmatrix, 0.001)
-}
-extension float2x2 : Equatable {}
-
-func beCloseTo(_ expectedValue: Matrix3x3Type, within delta: Double = 0.00001) -> NonNilMatcherFunc<Matrix3x3Type> {
-  return NonNilMatcherFunc<Matrix3x3Type> { (actualExpression:Expression<Matrix3x3Type>, failureMessage:FailureMessage) in
-    let actualValue = try! actualExpression.evaluate()!.cmatrix
-    failureMessage.actualValue = "\(actualValue)"
-    failureMessage.expected = "\(expectedValue.cmatrix)"
-    failureMessage.to = "be close"
-    return matrix_equal(actualValue, expectedValue.cmatrix)
-//    return matrix_almost_equal_elements(actualValue, expectedValue.cmatrix, ScalarType(delta))
-  }
-}
-
-func equal(expectedValue: Vector3Type) -> NonNilMatcherFunc<Vector3Type> {
-    return NonNilMatcherFunc { actualExpression, failureMessage in
-        let lhs = try! actualExpression.evaluate()!
-        let rhs = expectedValue
-        return lhs.x == rhs.x && lhs.y == rhs.y && lhs.z == rhs.z
-    }
-}
-
-func equal(expectedValue: Matrix3x3Type) -> NonNilMatcherFunc<Matrix3x3Type> {
-    return NonNilMatcherFunc { actualExpression, failureMessage in
-        return matrix_equal(try! actualExpression.evaluate()!.cmatrix, expectedValue.cmatrix)
+func beCloseTo(_ expectedValue: Matrix3x3Type, within delta: Double = 0.00001) -> Predicate<Matrix3x3Type> {
+    return Predicate<Matrix3x3Type> { actualExpression in
+        let actualValue = try! actualExpression.evaluate()!
+        return PredicateResult(
+            bool: simd_almost_equal_elements(actualValue, expectedValue, ScalarType(delta)),
+            message: ExpectationMessage.expectedActualValueTo("be close to \(expectedValue)")
+        )
     }
 }
 
