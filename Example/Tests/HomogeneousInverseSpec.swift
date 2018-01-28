b1dcd5bb191533b5cb38bd0f92c496a8b7f8564f
Paul Zabelin <pzabelin+git@gmail.com>
2018-01-28 03:12:25 -0800

    set development team to run tests on device
    reformat specs

--- Example/Tests/HomogeneousInverseSpec.swift
+++ Example/Tests/HomogeneousInverseSpec.swift
@@ -7,12 +7,12 @@ class HomogeneousInverseSpec: QuickSpec {
     override func spec() {
         describe("homogeneousInverse") {
             context("multiply adj by vector") {
-                let m = Matrix3x3Type([-122, -152, 18544,
-                                       122, 0, 0,
-                                       0, 152, 0])
-                let expected = Matrix3x3Type([[0.0, 0.0, 1.0],
-                                              [152.0, -0.0, 1.0],
-                                              [-0.0, 122.0, 1.0]])
+                let m = Matrix3x3Type([-122, -152, 122 * 152.0,
+                                        122,    0, 0,
+                                          0,  152, 0])
+                let expected = Matrix3x3Type([[  0, 0, 1],
+                                              [152, 0, 1],
+                                              [0, 122, 1]])
 
                 it("should match expected") {
                     expect(m.homogeneousInverse().zNormalized()) ≈ expected
--- Example/Tests/VectorProjectionSpec.swift
+++ Example/Tests/VectorProjectionSpec.swift
@@ -4,14 +4,14 @@ import simd
 
 class VectorProjectionSpec: QuickSpec {
     override func spec() {
-        describe("vector projection") { () -> () in
-            it("should be 0 orthogonally", closure: { () -> () in
+        describe("vector projection") {
+            it("should be 0 orthogonally") {
                 let v12 = vector_float2(1,0)
                 let v23 = vector_float2(0,1)
                 let p = project(v12, v23)
                 expect(p.x) == 0
                 expect(p.y) == 0
-            })
+            }
         }
     }
 }
