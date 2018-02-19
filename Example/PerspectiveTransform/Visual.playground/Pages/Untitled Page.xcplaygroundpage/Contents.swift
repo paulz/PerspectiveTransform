9af41ea340ebeb4ed8ea9c62d3124328341abba1
Paul Zabelin <pzabelin+git@gmail.com>
2018-02-18 22:56:30 -0800

    extract private function

---
+++ Example/PerspectiveTransform/Visual.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,8 @@
+//: [Previous](@previous)
+
+import Foundation
+import SourceKit
+
+var str = "Hello, playground"
+
+//: [Next](@next)
--- Pod/Classes/Perspective.swift
+++ Pod/Classes/Perspective.swift
@@ -15,20 +15,19 @@ public final class Perspective {
         vectors = q.corners.map{$0.homogeneous3dvector}
     }
 
-    lazy var basisVectorsToPointsMap: Matrix3x3Type! = {
-        let m = Matrix3x3Type(Array(vectors[0...2]))
-        let solution = m.homogeneousInverse() * vectors[3]
-        let scale = Matrix3x3Type(diagonal: solution)
-        let basisToPoints = m * scale
-        return basisToPoints.zNormalized()
-    }()
-
-    lazy var pointsToBasisVectorsMap: Matrix3x3Type! = {
-        basisVectorsToPointsMap.homogeneousInverse()
-    }()
+    lazy var basisVectorsToPointsMap: Matrix3x3Type = calculateBasisVectorsToPointsMap()
+    lazy var pointsToBasisVectorsMap: Matrix3x3Type = basisVectorsToPointsMap.homogeneousInverse()
 
     internal func projection(to:Perspective) -> Matrix3x3Type {
         return (to.basisVectorsToPointsMap * pointsToBasisVectorsMap).zNormalized()
+    }
+
+    private func calculateBasisVectorsToPointsMap() -> Matrix3x3Type {
+        let baseVectors = Matrix3x3Type(Array(vectors[0...2]))
+        let solution = baseVectors.homogeneousInverse() * vectors[3]
+        let scale = Matrix3x3Type(diagonal: solution)
+        let basisToPoints = baseVectors * scale
+        return basisToPoints.zNormalized()
     }
 }
 
