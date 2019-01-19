be9caea4ef305c125a5436f06bc90ac7e5978349
Paul Zabelin <pzabelin+git@gmail.com>
2019-01-19 02:21:55 -0800

    compare with tolerance on iPhone 5s

--- Example/Tests/SnapshotSpec.swift
+++ Example/Tests/SnapshotSpec.swift
@@ -6,7 +6,7 @@ import Foundation
 
 class SnapshotSpec: QuickSpec {
     override func spec() {
-        let runningOnTravisCI = ProcessInfo.processInfo.environment["TRAVIS"] != nil
+        let runningOnIphone5s = ProcessInfo.processInfo.environment["SIMULATOR_DEVICE_NAME"] == "iPhone 5s"
 
         describe("overlay placement") {
             var containerView : UIView!
@@ -41,15 +41,15 @@ class SnapshotSpec: QuickSpec {
 
                 it("should look as expected") {
                     var tolerance: CGFloat = 0
-                    let toleranceMessage = "\(100 * tolerance)% tolerance"
-                    if runningOnTravisCI {
+                    let toleranceMessage = {"\(100 * tolerance)% tolerance"}
+                    if runningOnIphone5s {
                         // For some unknown reason there is a 30% difference in image when running on Travis CI
                         // TODO: use pixel difference tolerance instead of image percent tolerance when it becomes available
                         // see https://github.com/uber/ios-snapshot-test-case/blob/master/CHANGELOG.md#502
                         tolerance = 0.3
-                        fputs("images compared on travis with \(toleranceMessage)\n".uppercased(), __stderrp)
+                        fputs("images compared on iPhone 5s with \(toleranceMessage())\n".uppercased(), __stderrp)
                     }
-                    expect(containerView).to(haveValidSnapshot(usesDrawRect:true, tolerance:tolerance), description: "should match with \(toleranceMessage)")
+                    expect(containerView).to(haveValidSnapshot(usesDrawRect:true, tolerance:tolerance), description: "should match with \(toleranceMessage())")
                 }
             }
         }
