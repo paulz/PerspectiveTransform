77780c4652f5adbd2fdcfec8a290fd92e0e995d0
Paul Zabelin <pzabelin+git@gmail.com>
2019-01-28 11:26:25 -0800

    refactor OpenCV wrapper to use static C functions

--- Example/OpenCV-OSX-Tests/OpenCVWrapper.mm
+++ Example/OpenCV-OSX-Tests/OpenCVWrapper.mm
@@ -11,46 +11,42 @@
 
 using namespace cv;
 
-@implementation OpenCVWrapper
+static Point2f convert(CGPoint point) {
+    return Point2f(point.x, point.y);
+}
 
-+ (CATransform3D)transform3DWithArray:(Mat)m {
+static Mat convert(Quadrilateral quad) {
+    Mat matrix = Mat();
+    matrix.push_back(convert(quad.upperLeft));
+    matrix.push_back(convert(quad.upperRight));
+    matrix.push_back(convert(quad.lowerRight));
+    matrix.push_back(convert(quad.lowerLeft));
+    return matrix;
+}
+
+static CATransform3D convert(Mat m) {
     CATransform3D transform = CATransform3DIdentity;
-    transform.m11 = m.at<double>(0, 0);
-    transform.m21 = m.at<double>(0, 1);
-    transform.m41 = m.at<double>(0, 2);
+    transform.m11 = m.at<CGFloat>(0, 0);
+    transform.m21 = m.at<CGFloat>(0, 1);
+    transform.m41 = m.at<CGFloat>(0, 2);
 
-    transform.m12 = m.at<double>(1, 0);
-    transform.m22 = m.at<double>(1, 1);
-    transform.m42 = m.at<double>(1, 2);
+    transform.m12 = m.at<CGFloat>(1, 0);
+    transform.m22 = m.at<CGFloat>(1, 1);
+    transform.m42 = m.at<CGFloat>(1, 2);
 
-    transform.m14 = m.at<double>(2, 0);
-    transform.m24 = m.at<double>(2, 1);
-    transform.m44 = m.at<double>(2, 2);
-
+    transform.m14 = m.at<CGFloat>(2, 0);
+    transform.m24 = m.at<CGFloat>(2, 1);
+    transform.m44 = m.at<CGFloat>(2, 2);
     return transform;
 }
 
-+ (std::vector<Point2f>)vectorWithQuadrilateral:(Quadrilateral)origin {
-    std::vector<Point2f> vec;
-    vec.push_back(Point2f(origin.upperLeft.x, origin.upperLeft.y));
-    vec.push_back(Point2f(origin.upperRight.x, origin.upperRight.y));
-    vec.push_back(Point2f(origin.lowerRight.x, origin.lowerRight.y));
-    vec.push_back(Point2f(origin.lowerLeft.x, origin.lowerLeft.y));
-    return vec;
-}
-
+@implementation OpenCVWrapper
 
 + (CATransform3D)findHomographyFromQuadrilateral:(Quadrilateral)origin toQuadrilateral:(Quadrilateral)destination {
-    std::vector<Point2f> start = [self vectorWithQuadrilateral:origin];
-    std::vector<Point2f> end = [self vectorWithQuadrilateral:destination];
-    Mat H = findHomography(start, end);
-    return [self transform3DWithArray:H];
+    return convert(findHomography(convert(origin), convert(destination)));
 }
 
 + (CATransform3D)perspectiveTransform:(Quadrilateral)origin toQuadrilateral:(Quadrilateral)destination {
-    std::vector<Point2f> start = [self vectorWithQuadrilateral:origin];
-    std::vector<Point2f> end = [self vectorWithQuadrilateral:destination];
-    Mat H = getPerspectiveTransform(start, end);
-    return [self transform3DWithArray:H];
+    return convert(getPerspectiveTransform(convert(origin), convert(destination)));
 }
 @end
