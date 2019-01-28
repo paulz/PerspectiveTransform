0d09b24a2cecdda61b415d9fb70514729032bd36
Paul Zabelin <pzabelin+git@gmail.com>
2019-01-27 21:50:08 -0800

    update opencv installation script for OpenCV 4

--- Example/OpenCV-OSX-Tests/install-opencv.sh
+++ Example/OpenCV-OSX-Tests/install-opencv.sh
@@ -2,9 +2,9 @@
 
 #  install-opencv.sh
 
-libraries="ilmbase snappy lame opencore-amr speex theora rtmpdump x264 x265 openssl libogg libvorbis ffmpeg jpeg libpng libtiff openexr opus opencv@2"
-for package in $libraries; do brew link $package || brew install --ignore-dependencies $package; done
+libraries="tbb openblas gcc ffmpeg libpng libtiff ilmbase openexr jpeg opencore-amr snappy lame opus speex theora libogg libvorbis x264 x265 rtmpdump openssl"
 
+for package in opencv $libraries; do brew link $package || brew install --ignore-dependencies --force-bottle $package; done
 
 # To remove all dependencies:
-# for package in opencv@2 xz $(brew deps opencv@2); do brew rm --ignore-dependencies $package; done
+# for package in opencv $(brew deps opencv); do brew rm --ignore-dependencies $package; done
