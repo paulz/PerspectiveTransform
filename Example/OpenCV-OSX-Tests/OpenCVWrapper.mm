822d7cf90f334c052330b7de96d17385c23cd465
Paul Zabelin <paulz@users.noreply.github.com>
2019-01-28 11:36:05 -0800

    Use OpenCV version 4 (#5)
    * use opencv4 
    * update opencv installation script for OpenCV 4
    * update directories for caching homebrew packages
    * add include path for mac os high sierra and opencv3
    * refactor OpenCV wrapper to use static C functions

--- Example/OpenCV-OSX-Tests/OpenCVWrapper.mm
+++ Example/OpenCV-OSX-Tests/OpenCVWrapper.mm
@@ -9,76 +9,44 @@
 #pragma GCC diagnostic pop
 #import "OpenCVWrapper.h"
 
-@implementation OpenCVWrapper
-+ (CATransform3D)findHomographyFromQuadrilateral:(Quadrilateral)origin toQuadrilateral:(Quadrilateral)destination {
-    CvPoint2D32f *cvsrc = [self openCVMatrixWithQuadrilateral:origin];
-    CvMat *src_mat = cvCreateMat( 4, 2, CV_32FC1 );
-    cvSetData(src_mat, cvsrc, sizeof(CvPoint2D32f));
+using namespace cv;
 
-    CvPoint2D32f *cvdst = [self openCVMatrixWithQuadrilateral:destination];
-    CvMat *dst_mat = cvCreateMat( 4, 2, CV_32FC1 );
-    cvSetData(dst_mat, cvdst, sizeof(CvPoint2D32f));
+static Point2f convert(CGPoint point) {
+    return Point2f(point.x, point.y);
+}
 
-    CvMat *H = cvCreateMat(3,3,CV_32FC1);
-    cvFindHomography(src_mat, dst_mat, H);
-    cvReleaseMat(&src_mat);
-    cvReleaseMat(&dst_mat);
+static Mat convert(Quadrilateral quad) {
+    Mat matrix = Mat();
+    matrix.push_back(convert(quad.upperLeft));
+    matrix.push_back(convert(quad.upperRight));
+    matrix.push_back(convert(quad.lowerRight));
+    matrix.push_back(convert(quad.lowerLeft));
+    return matrix;
+}
 
-    CATransform3D transform = [self transform3DWithCMatrix:H->data.fl];
-    cvReleaseMat(&H);
+static CATransform3D convert(Mat m) {
+    CATransform3D transform = CATransform3DIdentity;
+    transform.m11 = m.at<CGFloat>(0, 0);
+    transform.m21 = m.at<CGFloat>(0, 1);
+    transform.m41 = m.at<CGFloat>(0, 2);
 
+    transform.m12 = m.at<CGFloat>(1, 0);
+    transform.m22 = m.at<CGFloat>(1, 1);
+    transform.m42 = m.at<CGFloat>(1, 2);
+
+    transform.m14 = m.at<CGFloat>(2, 0);
+    transform.m24 = m.at<CGFloat>(2, 1);
+    transform.m44 = m.at<CGFloat>(2, 2);
     return transform;
+}
+
+@implementation OpenCVWrapper
+
++ (CATransform3D)findHomographyFromQuadrilateral:(Quadrilateral)origin toQuadrilateral:(Quadrilateral)destination {
+    return convert(findHomography(convert(origin), convert(destination)));
 }
 
 + (CATransform3D)perspectiveTransform:(Quadrilateral)origin toQuadrilateral:(Quadrilateral)destination {
-    CvPoint2D32f *cvsrc = [self openCVMatrixWithQuadrilateral:origin];
-    CvMat *src_mat = cvCreateMat( 4, 2, CV_32FC1 );
-    cvSetData(src_mat, cvsrc, sizeof(CvPoint2D32f));
-
-    CvPoint2D32f *cvdst = [self openCVMatrixWithQuadrilateral:destination];
-    CvMat *dst_mat = cvCreateMat( 4, 2, CV_32FC1 );
-    cvSetData(dst_mat, cvdst, sizeof(CvPoint2D32f));
-
-    CvMat *H = cvCreateMat(3,3,CV_32FC1);
-    cvGetPerspectiveTransform(cvsrc, cvdst, H);
-    cvReleaseMat(&src_mat);
-    cvReleaseMat(&dst_mat);
-
-    CATransform3D transform = [self transform3DWithCMatrix:H->data.fl];
-    cvReleaseMat(&H);
-
-    return transform;
-}
-
-+ (CvPoint2D32f *)openCVMatrixWithQuadrilateral:(Quadrilateral)origin {
-
-    CvPoint2D32f *cvsrc = (CvPoint2D32f *)malloc(4*sizeof(CvPoint2D32f));
-    cvsrc[0].x = origin.upperLeft.x;
-    cvsrc[0].y = origin.upperLeft.y;
-    cvsrc[1].x = origin.upperRight.x;
-    cvsrc[1].y = origin.upperRight.y;
-    cvsrc[2].x = origin.lowerRight.x;
-    cvsrc[2].y = origin.lowerRight.y;
-    cvsrc[3].x = origin.lowerLeft.x;
-    cvsrc[3].y = origin.lowerLeft.y;
-    return cvsrc;
-}
-
-+ (CATransform3D)transform3DWithCMatrix:(float *)matrix {
-    CATransform3D transform = CATransform3DIdentity;
-
-    transform.m11 = matrix[0];
-    transform.m21 = matrix[1];
-    transform.m41 = matrix[2];
-
-    transform.m12 = matrix[3];
-    transform.m22 = matrix[4];
-    transform.m42 = matrix[5];
-
-    transform.m14 = matrix[6];
-    transform.m24 = matrix[7];
-    transform.m44 = matrix[8];
-
-    return transform;
+    return convert(getPerspectiveTransform(convert(origin), convert(destination)));
 }
 @end
