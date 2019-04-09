d90988b427bb72d2a8538502724fbd7732fc3e0d
Paul Zabelin <pzabelin+git@gmail.com>
2019-04-09 04:27:46 -0700

    simplify build for testing

--- Example/OpenCV-OSX-Tests/install-opencv.sh
+++ Example/OpenCV-OSX-Tests/install-opencv.sh
@@ -2,9 +2,9 @@
 
 #  install-opencv.sh
 
-libraries="tbb openblas gcc ffmpeg libpng libtiff ilmbase openexr jpeg opencore-amr snappy lame opus speex theora libogg libvorbis x264 x265 rtmpdump openssl"
+libraries="tbb openblas gcc ffmpeg libpng libtiff ilmbase openexr jpeg opencore-amr snappy lame opus speex theora libogg libvorbis x264 x265 rtmpdump openssl libsoxr openjpeg libbluray gnutls p11-kit libunistring libffi gmp nettle libtasn1"
 
 for package in opencv $libraries; do brew link $package || brew install --ignore-dependencies --force-bottle $package; done
 
 # To remove all dependencies:
-# for package in opencv $(brew deps opencv); do brew rm --ignore-dependencies $package; done
+# for package in opencv $(brew deps opencv); do brew rm --ignore-dependencies --force $package; done
