04d5a5326cc4d69cfe1dd6f221ca1a9567a2cf3f
Paul Zabelin <pzabelin+git@gmail.com>
2019-04-09 04:50:55 -0700

    update opencv dependencies for mojave 10.14.4

--- Example/OpenCV-OSX-Tests/install-opencv.sh
+++ Example/OpenCV-OSX-Tests/install-opencv.sh
@@ -2,7 +2,7 @@
 
 #  install-opencv.sh
 
-libraries="tbb openblas gcc ffmpeg libpng libtiff ilmbase openexr jpeg opencore-amr snappy lame opus speex theora libogg libvorbis x264 x265 rtmpdump openssl libsoxr openjpeg libbluray gnutls p11-kit libunistring libffi gmp nettle libtasn1"
+  libraries="tbb openblas gcc ffmpeg libpng libtiff ilmbase openexr jpeg opencore-amr snappy lame openjpeg opus speex theora libogg libvorbis x264 x265 libsoxr libbluray gnutls rtmpdump openssl fontconfig freetype p11-kit libunistring libtasn1 nettle gmp libffi"
 
 for package in opencv $libraries; do brew link $package || brew install --ignore-dependencies --force-bottle $package; done
 
