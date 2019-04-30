035fcb83b94fce967f8fad8272d9db5f173691a9
Paul Zabelin <pzabelin+git@gmail.com>
2019-04-30 13:05:52 -0700

    ignore errors trying to restore links to openblas
    ln: /usr/local/opt/openblas/0.3.5: File exists
    https://travis-ci.org/paulz/PerspectiveTransform/jobs/526580145#L878

--- Example/OpenCV-OSX-Tests/install-opencv.sh
+++ Example/OpenCV-OSX-Tests/install-opencv.sh
@@ -7,7 +7,7 @@ libraries="tbb openblas gcc ffmpeg libpng libtiff ilmbase openexr jpeg opencore-
 for package in opencv $libraries; do brew link $package || brew install --ignore-dependencies --force-bottle $package; done
 # due to Warning: Refusing to link macOS-provided software: openblas
 # link /usr/local/opt/openblas is not getting restored
-ln -s /usr/local/Cellar/openblas/`ls /usr/local/Cellar/openblas` /usr/local/opt/openblas
+ln -s /usr/local/Cellar/openblas/`ls /usr/local/Cellar/openblas` /usr/local/opt/openblas || true
 
 # To remove all dependencies:
 # for package in opencv $(brew deps opencv); do brew rm --ignore-dependencies --force $package; done
