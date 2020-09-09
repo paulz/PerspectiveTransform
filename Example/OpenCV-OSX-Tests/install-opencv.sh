300e961fe40682621d5949697ed4176333456599
Paul Zabelin <paulz@users.noreply.github.com>
2020-09-09 01:05:51 -0700

    experiment with brew list

--- Example/OpenCV-OSX-Tests/install-opencv.sh
+++ Example/OpenCV-OSX-Tests/install-opencv.sh
@@ -4,14 +4,7 @@
 
 libraries="openblas tbb gcc"
 
-# due to Warning: Refusing to link macOS-provided software: openblas
-# link /usr/local/opt/openblas is not getting restored
-ln -s /usr/local/Cellar/openblas/`ls /usr/local/Cellar/openblas` /usr/local/opt/openblas || true
-
 for package in $libraries opencv
 do
-  brew link $package || brew install --ignore-dependencies --force-bottle $package
+  brew list $package || brew install --ignore-dependencies --force-bottle $package
 done
-
-# To remove all dependencies:
-# for package in opencv $(brew deps opencv); do brew rm --ignore-dependencies --force $package; done
